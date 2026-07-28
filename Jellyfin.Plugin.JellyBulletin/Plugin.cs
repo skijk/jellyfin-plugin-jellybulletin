05c19e65d2553f04d39b0edcd7760ae19e52f06d
skijk <95034810+skijk@users.noreply.github.com>
2026-07-28 19:42:11 +0200

    Add Jellyfin.Plugin.JellyBulletin/Plugin.cs

---
+++ Jellyfin.Plugin.JellyBulletin/Plugin.cs
@@ -0,0 +1,43 @@
+using System.Globalization;
+using Jellyfin.Plugin.JellyBulletin.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Jellyfin.Plugin.JellyBulletin;
+
+/// <summary>
+/// JellyBulletin plugin.
+/// </summary>
+public sealed class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+{
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
+    {
+        Instance = this;
+    }
+
+    public override string Name => "JellyBulletin";
+
+    public override string Description => "News and announcements for Jellyfin.";
+
+    public override Guid Id => Guid.Parse("6ad77d9a-e157-4ca2-82e7-a114f86a5f50");
+
+    public static Plugin Instance { get; private set; } = null!;
+
+    public IEnumerable<PluginPageInfo> GetPages()
+    {
+        return
+        [
+            new PluginPageInfo
+            {
+                Name = Name,
+                EmbeddedResourcePath = string.Format(
+                    CultureInfo.InvariantCulture,
+                    "{0}.Configuration.configPage.html",
+                    GetType().Namespace)
+            }
+        ];
+    }
+}
