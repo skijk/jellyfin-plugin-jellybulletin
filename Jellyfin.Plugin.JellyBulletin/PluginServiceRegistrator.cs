b0a62e5fb6c27e688d92408998df354981913387
skijk <95034810+skijk@users.noreply.github.com>
2026-07-28 19:42:28 +0200

    Add Jellyfin.Plugin.JellyBulletin/PluginServiceRegistrator.cs

---
+++ Jellyfin.Plugin.JellyBulletin/PluginServiceRegistrator.cs
@@ -0,0 +1,17 @@
+using Jellyfin.Plugin.JellyBulletin.Services;
+using MediaBrowser.Controller;
+using MediaBrowser.Controller.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jellyfin.Plugin.JellyBulletin;
+
+/// <summary>
+/// Registers plugin services.
+/// </summary>
+public sealed class PluginServiceRegistrator : IPluginServiceRegistrator
+{
+    public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
+    {
+        serviceCollection.AddSingleton<BulletinStore>();
+    }
+}
